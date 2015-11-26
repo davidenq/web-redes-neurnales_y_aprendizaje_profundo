61107e9ebefa7102e28836055b896ed925ef4956
davidenq <davip@live.com>
2015-11-25 21:26:02 -0500

    the router.js file was renamed to routes.js

--- app/router.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const server = require('../server');
-const controllers = server.settings.app.modules.controllers;
-
-server.route([{
-    method: 'GET',
-    path: '/',
-    handler: controllers.index.home
-}]);
