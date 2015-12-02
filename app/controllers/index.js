e09522e89e96e71aefd4112d186278393b0e3017
davidenq <davip@live.com>
2015-12-02 08:07:21 -0500

    update project

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const content = require('../markdown-book');
-const template = require('../resources/index.marko');
+const content = require('../book');
+const index = require('../resources/index.marko');
+const chapter1 = require('../resources/chapter1.marko');
 
 exports.index = function(request, reply) {
-    template.render(content.index, reply);
+    index.render(content.index, reply);
 };
 exports.about = function(request, reply) {
-    template.render(content.about, reply);
+    index.render(content.about, reply);
 };
 exports.exercises = function(request, reply) {
-    template.render(content.exercises, reply);
+    index.render(content.exercises, reply);
 };
 exports.chapter1 = function(request, reply) {
-    template.render(content.chapter1, reply);
+    chapter1.render(content.chapter1, reply);
 };
